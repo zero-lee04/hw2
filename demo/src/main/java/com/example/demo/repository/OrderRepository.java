7a2073f20457704ef32f1bca8077cbd3010725c1
zero <kaka0a02@naver.com>
2025-12-15 02:40:58 +0900

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Orders, Long> {
-}
--- demo/src/main/java/com/example/demo/repository/ReviewCommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.domain.ReviewComment;
-
-public interface ReviewCommentRepository extends JpaRepository<ReviewComment, Long> {
-}
